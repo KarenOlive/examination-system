e8eccf0069d00a73b44207447388b8d371864e7b
ridhwan497 <ridubangire@gmail.com>
2025-03-23 01:23:42 +0300

    Basically testing if the workflow i created is working

---
+++ src/test/java/com/example/TestingWorkflow.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExampleTest {
+    @Test
+    void failingTest() {
+        assertEquals(1, 2);  // This will fail hopefully :)
+    }
+}
+
